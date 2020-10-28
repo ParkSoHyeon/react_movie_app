fb16439f73a265e043e2447d0e600dc65b26a808
parksohyeon <dso753@elinmedia.com>
2020-10-28 19:07:58 +0900

    Movie App 만들기 commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import React from 'react'
+import axios from 'axios'
+import Movie from "./Movie";
+import './App.css'
 
-// react는 자동적으로 class component의 render method를 실행함.
+// react는 자동적으로 className component의 render method를 실행함.
 class App extends React.Component {
     state = {
-        isLoading: true
+        isLoading: true,
+        movies: []
     }
+    getMovies = async () => {
+        const {data: {data: {movies}}} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating')
+        this.setState({movies, isLoading: false})
+        console.log(movies);
+    }
+
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({isLoading: false});
-        }, 6000)
+        this.getMovies();
     }
+
     render() {
-        const { isLoading } = this.state;
+        const {isLoading, movies} = this.state;
         return (
-            <div>{isLoading ? 'Loading...' : 'We are ready!'}</div>
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="leader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map(movie => {
+                            console.log(movie);
+                            return <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        })}
+                    </div>
+                )}
+            </section>
         )
+
     }
 }
 
