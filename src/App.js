d5710973f38b3dda42de4655fec464dadbc9aa0f
parksohyeon <dso753@elinmedia.com>
2020-10-28 10:35:12 +0900

    state, component life cycle 예제 commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import React from 'react'
 
 const foodILike = [
     {
@@ -54,13 +55,34 @@ Food.propTypes = {
     rating: PropTypes.number,
 }
 
-function App() {
-  return (
-    <div className="App">
-        { foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />) }
-    </div>
-
-  );
+// react는 자동적으로 class component의 render method를 실행함.
+class App extends React.Component {
+    state = {
+        count: 0
+    }
+    add = () => {
+        // this.setState({count: this.state.count + 1 });
+        this.setState(current => ({count: current.count + 1}));
+    }
+    minus = () => {
+        // this.setState({count: this.state.count - 1 });
+        this.setState(current => ({count: current.count - 1}));
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+    render() {
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        )
+    }
 }
 
 export default App;
